92abccec0dbe2c514e2aa6aff1db27c679873a35
sunyoung315 <rlatjsdud315@gmail.com>
2024-01-22 00:04:15 +0900

    [sunyoung]Feat: Figma 컬러 추출

--- frontend/saessak/src/components/board/BoardView.vue
+++ frontend/saessak/src/components/board/BoardView.vue
@@ -1,26 +1,33 @@
 <template>
-	<div class="container p-6">
-		<h1 class="text-4xl font-extrabold mt-6 mr-6 ml-6 inline-block">알림장</h1>
+	<div class="container p-6 flex items-center">
+		<span class="text-4xl m-5 font-extrabold inline-block">알림장</span>
 		<div class="inline-flex rounded-md shadow-sm m-6">
-			<RouterLink
-				to="/summary"
+			<button
+				@click="summaryModal.openModal"
 				aria-current="page"
-				class="px-6 py-3 text-m font-medium font-bold text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-900 focus:z-10 focus:ring-2 focus:ring-blue-900 focus:text-blue-900"
+				class="px-6 py-3 text-m font-bold text-dark-navy bg-white border border-gray-200 rounded-s-lg hover:bg-dark-navy hover:text-white focus:z-10 focus:ring-2 focus:ring-dark-navy focus:text-dark-navy"
 			>
 				요약레포트
-			</RouterLink>
-			<RouterLink
-				to="/growth"
-				class="px-6 py-3 text-m font-medium font-bold text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-900 focus:z-10 focus:ring-2 focus:ring-blue-900 focus:text-blue-900"
+			</button>
+			<button
+				@click="growthModal.openModal"
+				class="px-6 py-3 text-m font-bold text-dark-navy bg-white border border-gray-200 rounded-e-lg hover:bg-dark-navy hover:text-white focus:z-10 focus:ring-2 focus:ring-dark-navy focus:text-dark-navy"
 			>
 				성장 기록 확인
-			</RouterLink>
+			</button>
 		</div>
-
-		<RouterView></RouterView>
+		<BoardSummaryModal ref="summaryModal" />
+		<BoardGrowthModal ref="growthModal" />
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import BoardSummaryModal from '@/components/board/BoardSummaryModal.vue';
+import BoardGrowthModal from '@/components/board/BoardGrowthModal.vue';
+
+const summaryModal = ref();
+const growthModal = ref();
+</script>
 
 <style scoped></style>
--- frontend/saessak/tailwind.config.js
+++ frontend/saessak/tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'nav-red': '#D7A2A0',
+				'nav-orange': '#E8C8A0',
+				'nav-yellow': '#F5E9A8',
+				'nav-green': '#A0C09B',
+				'nav-blue': '#A2B6CD',
+				'nav-navy': '#8B91B9',
+				'nav-purple': '#A68BB4',
+				'dark-navy': '#333752',
+				'yellow-bg1': '#FFFDF4',
+				'yellow-bg2': '#FFF4B7',
+			},
+		},
 	},
 	variants: {
 		extend: {},
