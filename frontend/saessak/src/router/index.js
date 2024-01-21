2d60b4f814db4c928ad827a34c64ab1e0883e884
manju0329 <gabs3798@gmail.com>
2024-01-22 00:59:22 +0900

    [minju]Feat: chat 기본 UI 작성

--- frontend/saessak/src/router/index.js
+++ frontend/saessak/src/router/index.js
@@ -1,24 +1,85 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import ChatView from "@/views/ChatView.vue";
-import ChatDetail from "@/views/ChatDetailView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HomeView from '@/views/HomeView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import LoginView from '@/views/LoginView.vue';
+import UserView from '@/components/user/UserView.vue';
+import BoardView from '@/components/board/BoardView.vue';
+import AlbumView from '@/components/album/AlbumView.vue';
+import NoticeView from '@/components/notice/NoticeView.vue';
+import DocumentView from '@/components/document/DocumentView.vue';
+import MenuView from '@/components/menu/MenuView.vue';
+import AttendanceView from '@/components/attendance/AttendanceView.vue';
+import ChatView from '@/components/chat/ChatView.vue';
+import ChatDetail from "@/components/ChatDetailView.vue";
+import SettingView from '@/components/setting/SettingView.vue';
+
+const routes = [
+	{ path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
+	{
+		path: '/',
+		name: 'Home',
+		component: HomeView,
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: LoginView,
+	},
+	{
+		path: '/user',
+		name: 'User',
+		component: UserView,
+	},
+	{
+		path: '/board',
+		name: 'Board',
+		component: BoardView,
+	},
+	{
+		path: '/album',
+		name: 'Album',
+		component: AlbumView,
+	},
+	{
+		path: '/notice',
+		name: 'Notice',
+		component: NoticeView,
+	},
+	{
+		path: '/document',
+		name: 'Document',
+		component: DocumentView,
+	},
+	{
+		path: '/menu',
+		name: 'Menu',
+		component: MenuView,
+	},
+	{
+		path: '/attendance',
+		name: 'Attendance',
+		component: AttendanceView,
+	},
+	{
+		path: '/chat',
+		name: 'Chat',
+		component: ChatView,
+	},
+	{
+		path: "/chat/:id",
+		component: ChatDetail
+	}, // /chat/:id에 대한 동적 라우트
+	{
+		path: '/setting',
+		name: 'Setting',
+		component: SettingView,
+	},
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    { path: "/", component: HomeView },
-
-    { path: "/chat", component: ChatView }, // /chat에 대한 기본 컴포넌트
-    { path: "/chat/:id", component: ChatDetail }, // /chat/:id에 대한 동적 라우트
-    // {
-    //   path: '/chat',
-    //   component: ChatView,
-    //   children: [
-    //     { path: '', component: ChatView }, // /chat에 대한 기본 컴포넌트
-    //     { path: '/:id', component: ChatDetail }, // /chat/:id에 대한 동적 라우트
-    //   ],
-    // },
-  ],
+	history: createWebHistory('/'),
+	routes,
 });
 
 export default router;
