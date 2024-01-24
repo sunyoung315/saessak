98f52164a30ed24ad2daa156d023eb58d8529124
이민주 <gabs3798@naver.com>
2024-01-24 17:35:57 +0900

    [minju]Feat:채팅 API 연동 및 소켓 진행

--- frontend/saessak/tailwind.config.js
+++ frontend/saessak/tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
 	// purge: [],
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-	darkMode: false, // or 'media' or 'class'
+	darkMode: false, // or 'media' or 'class'    
+	content: [
+        "./node_modules/flowbite/**/*.js"
+    ],
 	theme: {
 		extend: {
 			colors: {
@@ -22,5 +25,6 @@ module.exports = {
 	variants: {
 		extend: {},
 	},
-	plugins: [require("tailwind-scrollbar-hide")]
+	plugins: [require("tailwind-scrollbar-hide"), 
+	require('flowbite/plugin')]
 };
