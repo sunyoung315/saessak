2040296f2f522ab03f11386415ddb20f4cae79ac
이민주 <gabs3798@naver.com>
2024-01-26 17:44:22 +0900

    [minju]Feat:카카오 로그인 API 추가

--- frontend/saessak/src/components/common/TheHeader.vue
+++ frontend/saessak/src/components/common/TheHeader.vue
@@ -16,6 +16,13 @@
           채팅
         </button>
         <RouterLink to="/alram">알람</RouterLink>
+        <button
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          type="button"
+          @click="login()"
+        >
+          로그인
+        </button>
       </div>
     </div>
   </div>
@@ -62,6 +69,7 @@
 <script setup>
 import { onMounted, shallowRef, ref, watch } from 'vue';
 import { initFlowbite } from 'flowbite';
+import { kakaoLogin } from '@/api/oauth';
 import ChatListView from "@/components/chat/ChatListView.vue";
 import ChatPersonView from "@/components/chat/ChatPersonView.vue";
 import ChatDetailView from '../chat/ChatDetailView.vue';
@@ -88,6 +96,12 @@ const showChat = (name) => {
   chatSwitch.value = name;
   console.log(chatSwitch.value);
 };
+
+const login = () => {
+  kakaoLogin(({data}) => {
+    console.log(data);
+  })
+}
 </script>
 
 <style scoped>
