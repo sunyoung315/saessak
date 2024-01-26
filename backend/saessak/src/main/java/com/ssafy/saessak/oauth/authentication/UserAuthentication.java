b8531413e50f0fa0911363f09f04c3627ca80994
gw07167 <gw07167@naver.com>
2024-01-26 14:27:21 +0900

    [suna]Refeat: 식단 사진 AWS S3 업로드 기능 구현

--- backend/saessak/src/main/java/com/ssafy/saessak/oauth/authentication/UserAuthentication.java
+++ backend/saessak/src/main/java/com/ssafy/saessak/oauth/authentication/UserAuthentication.java
@@ -1,7 +1,9 @@
 package com.ssafy.saessak.oauth.authentication;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Collection;
 
@@ -10,6 +12,11 @@ public class UserAuthentication extends UsernamePasswordAuthenticationToken {
     // 사용자 인증 객체 생성
     public UserAuthentication(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
         super(principal, credentials, authorities);
+    }
+
+    public getUserByAuthentication() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
     }
 
 }
