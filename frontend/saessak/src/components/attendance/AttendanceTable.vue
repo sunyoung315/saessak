8baa0a8f156acb94ab53b6ba01e27e968b04e1fb
sunyoung315 <rlatjsdud315@gmail.com>
2024-01-28 17:26:48 +0900

    [sunyoung]feat: 출석부 기능구현 로컬 테스트 완료, 학부모ver 달력 view 변경

--- frontend/saessak/src/components/attendance/AttendanceTable.vue
+++ frontend/saessak/src/components/attendance/AttendanceTable.vue
@@ -1,161 +1,174 @@
 <template>
-	<div
-		class="container mx-16 mb-10 p-8 w-auto border border-gray-200 shadow rounded-lg"
-	>
+	<div class="view-frame">
 		<!-- datepicker -->
 		<div class="flex justify-between items-center m-5">
-			<input
-				type="date"
+			<VDatePicker
+				:select-attribute="selectAttribute"
 				v-model="selectedDate"
 				@change="getWeekOfMonth"
-				class="inline-block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-36 py-2 px-4"
-			/>
+			>
+				<template #default="{ inputValue, inputEvents }">
+					<div class="relative max-w-sm">
+						<div
+							class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none"
+						>
+							<svg
+								class="w-4 h-4 text-gray-900 dark:text-gray-400"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+							>
+								<path
+									d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+								/>
+							</svg>
+						</div>
+						<input
+							:value="inputValue"
+							v-on="inputEvents"
+							class="border border-gray-300 text-gray-900 text-sm font-bold rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
+						/>
+					</div>
+				</template>
+			</VDatePicker>
 			<p class="inline-block w-48 font-bold text-xl">{{ weekly }}</p>
 			<p class="inline-block w-36"></p>
 		</div>
 		<!-- table -->
-		<div class="block">
-			<div class="relative overflow-x-auto min-h-screen">
-				<table
-					class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-				>
-					<thead
-						class="text-xs text-gray-700 bg-nav-purple dark:bg-gray-700 dark:text-gray-400"
+		<div class="table-box">
+			<table class="table">
+				<thead class="table-head">
+					<tr>
+						<th scope="col" class="col-name">이름</th>
+						<th scope="col" class="col-date">
+							{{ formattedWeekDays[0] }} (월)
+						</th>
+						<th scope="col" class="col-date">
+							{{ formattedWeekDays[1] }} (화)
+						</th>
+						<th scope="col" class="col-date">
+							{{ formattedWeekDays[2] }} (수)
+						</th>
+						<th scope="col" class="col-date">
+							{{ formattedWeekDays[3] }} (목)
+						</th>
+						<th scope="col" class="col-date">
+							{{ formattedWeekDays[4] }} (금)
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr
+						class="one-row"
+						v-for="oneKid in attendanceList"
+						:key="oneKid.kidId"
 					>
-						<tr>
-							<th scope="col" class="pl-3 pr-6 py-3 w-1/12">이름</th>
-							<th scope="col" class="px-6 py-3 w-1/6">
-								{{ formattedWeekDays[0] }} (월)
-							</th>
-							<th scope="col" class="px-6 py-3 w-1/6">
-								{{ formattedWeekDays[1] }} (화)
-							</th>
-							<th scope="col" class="px-6 py-3 w-1/6">
-								{{ formattedWeekDays[2] }} (수)
-							</th>
-							<th scope="col" class="px-6 py-3 w-1/6">
-								{{ formattedWeekDays[3] }} (목)
-							</th>
-							<th scope="col" class="px-6 py-3 w-1/6">
-								{{ formattedWeekDays[4] }} (금)
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr
-							class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-							v-for="oneKid in attendanceList"
-							:key="oneKid.kidId"
+						<!-- 아이 이름 -->
+						<th scope="row" class="kid-name">
+							{{ oneKid.kidName }}
+						</th>
+						<td
+							class="pl-4"
+							v-for="formattedDate in formattedDates"
+							:key="formattedDate"
 						>
-							<!-- 아이 이름 -->
-							<th
-								scope="row"
-								class="pl-3 pr-6 py-4 font-semibold text-gray-900 whitespace-nowrap"
+							<template
+								v-for="attendance in oneKid.attendance"
+								:key="attendance.attendanceId"
 							>
-								{{ oneKid.kidName }}
-							</th>
-							<td
-								class="pl-3 pr-2"
-								v-for="formattedDate in formattedDates"
-								:key="formattedDate"
-							>
-								<template v-if="today === formattedDate">
-									<!-- 버튼 -->
-									<div class="flex pl-3 pr-2 py-2">
-										<!-- 등원 버튼 누르기 전 -->
-										<div>
-											<button
-												type="submit"
-												class="w-16 my-2 mr-2 p-2 border rounded-md text-center h-9 align-center font-semibold bg-dark-navy text-white"
-												@click="inTimePush(oneKid.kidId)"
-											>
-												등원
-											</button>
-										</div>
-										<!-- 등원 버튼 누른 후 -->
-										<!-- <div>
-											<p
-												class="my-2 mr-2 p-2 border rounded-md w-16 text-center font-semibold"
-											>
-												{{ todayInTime }}
-											</p>
-										</div> -->
-										<!-- 하원 버튼 누르기 전 -->
-										<!-- <div v-if="!inTime"> -->
-										<button
-											type="submit"
-											class="w-16 my-2 mr-2 p-2 border rounded-md text-center h-9 align-center font-semibold bg-dark-navy text-white"
-											@click="outTimePush(oneKid.kidId)"
-										>
-											하원
-										</button>
-										<!-- </div> -->
-										<!-- 하원 버튼 누른 후 -->
-										<!-- <div v-else>
-											<p
-												class="my-2 mr-2 p-2 border rounded-md w-16 text-center font-semibold"
-											>
-												{{ todayOutTime }}
-											</p>
-										</div> -->
-									</div>
-								</template>
-								<template v-else>
-									<template
-										v-for="attendance in oneKid.attendance"
-										:key="attendance.attendanceId"
-									>
-										<template
-											v-if="formattedDate === attendance.attendanceDate"
-										>
-											<!-- 아이 출석 기록 -->
-											<template v-if="attendance.attendanceInTime">
-												<!-- 1. 등원O 하원X -->
-												<template v-if="!attendance.attendanceOutTime">
-													<div class="flex">
-														<p
-															class="my-2 mr-2 p-2 border rounded-md w-16 text-center font-semibold"
-														>
-															{{ attendance.attendanceInTime }}
-														</p>
-														<p
-															class="my-2 p-2 border rounded-md w-16 text-center font-semibold text-gray-400"
-														>
-															미입력
-														</p>
-													</div>
-												</template>
-												<!-- 등원O, 하원O -->
-												<template v-else>
-													<div class="flex">
-														<p
-															class="my-2 mr-2 p-2 border rounded-md w-16 text-center font-semibold"
-														>
-															{{ attendance.attendanceInTime }}
-														</p>
-														<p
-															class="my-2 p-2 border rounded-md w-16 text-center font-semibold"
-														>
-															{{ attendance.attendanceOutTime }}
-														</p>
-													</div>
-												</template>
-											</template>
-											<template v-else>
-												<p
-													class="my-2 mr-2 p-2 border rounded-md w-16 text-center font-semibold text-red-400"
-												>
-													결석
+								<!-- 과거 날짜 -->
+								<template
+									v-if="
+										formattedDate !== today &&
+										formattedDate === attendance.attendanceDate
+									"
+								>
+									<!-- 아이 출석 기록 -->
+									<template v-if="attendance.attendanceInTime">
+										<!-- 1. 등원O 하원X -->
+										<template v-if="!attendance.attendanceOutTime">
+											<div class="flex">
+												<p class="normal-value">
+													{{ attendance.attendanceInTime }}
 												</p>
-											</template>
+												<p class="miss-value">미입력</p>
+											</div>
+										</template>
+										<!-- 등원O, 하원O -->
+										<template v-else>
+											<div class="flex">
+												<p class="normal-value">
+													{{ attendance.attendanceInTime }}
+												</p>
+												<p class="normal-value">
+													{{ attendance.attendanceOutTime }}
+												</p>
+											</div>
 										</template>
 									</template>
+									<template v-else>
+										<p class="no-value">결석</p>
+									</template>
 								</template>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+							</template>
+							<!-- 오늘 날짜 -->
+							<div v-if="today === formattedDate" class="flex">
+								<!-- 버튼 -->
+								<template
+									v-if="
+										!store.todayInTimeList.find(
+											one => one.kidId === oneKid.kidId,
+										)
+									"
+								>
+									<!-- 등원 버튼 누르기 전 -->
+									<button
+										type="submit"
+										class="button"
+										@click="inTimePush(oneKid.kidId)"
+									>
+										등원
+									</button>
+								</template>
+								<template v-else>
+									<p class="normal-value">
+										{{
+											store.todayInTimeList.find(
+												one => one.kidId === oneKid.kidId,
+											).inTime
+										}}
+									</p>
+								</template>
+								<template
+									v-if="
+										!store.todayOutTimeList.find(
+											one => one.kidId === oneKid.kidId,
+										)
+									"
+								>
+									<button
+										type="submit"
+										class="button"
+										@click="outTimePush(oneKid.kidId)"
+									>
+										하원
+									</button>
+								</template>
+								<template v-else>
+									<p class="normal-value">
+										{{
+											store.todayOutTimeList.find(
+												one => one.kidId === oneKid.kidId,
+											).outTime
+										}}
+									</p>
+								</template>
+							</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </template>
@@ -164,13 +177,14 @@
 import { ref, computed, watch, onMounted } from 'vue';
 import { getWeekOfMonth, getMonth, addDays, format } from 'date-fns';
 import { useAttendanceStore } from '@/store/attendance.js';
-import { kidIn, kidOut } from '@/api/attendance.js';
 
 // input type="date"는 "yyyy-MM-dd" 형식의 문자열 -> 같은 형식으로 변환
 let selectedDate = ref(new Date().toISOString().split('T')[0]);
 
 // 오늘 날짜
-const today = format(new Date(), 'yyyy-MM-dd');
+const today = ref(format(new Date(), 'yyyy-MM-dd'));
+
+const selectAttribute = ref({ highlight: 'purple' });
 
 // 해당년도, 월, 주차
 const year = ref();
@@ -187,7 +201,7 @@ watch(
 		if (newVal) {
 			let date = new Date(newVal);
 			// 일요일 기준(0), 월요일 기준(1)
-			weekOfMonth.value = getWeekOfMonth(date, { weekStartsOn: 0 });
+			weekOfMonth.value = getWeekOfMonth(date, { weekStartsOn: 1 });
 			month.value = getMonth(date) + 1;
 			year.value = date.getFullYear();
 
@@ -222,7 +236,7 @@ let weekly = computed(() => {
 const store = useAttendanceStore();
 
 // 임시 반 데이터
-const classroomId = 1;
+const classroomId = 2;
 ////////////////
 
 // 주차별 출석부 요청 data
@@ -230,7 +244,7 @@ const listData = ref({
 	classroomId,
 	year,
 	month,
-	week: weekOfMonth,
+	week: weekOfMonth.value,
 });
 
 // 주차별 출석부
@@ -242,22 +256,52 @@ const getList = async () => {
 	attendanceList.value = store.weeklyList;
 };
 
+const inTimePush = async kidId => {
+	await store.kidIn(kidId);
+};
+
+const outTimePush = async kidId => {
+	await store.kidOut(kidId);
+};
+
 // 오늘 날짜(기본값) 데이터로 마운트
 onMounted(async () => {
 	await getList();
 });
-
-const inTimePush = async kidId => {
-	await kidIn(kidId);
-	await store.getWeeklyList(listData.value);
-	attendanceList.value = store.weeklyList;
-};
-
-const outTimePush = async kidId => {
-	await kidOut(kidId);
-	await store.getWeeklyList(listData.value);
-	attendanceList.value = store.weeklyList;
-};
 </script>
 
-<style scoped></style>
+<style scoped>
+.table-box {
+	@apply relative overflow-x-auto min-h-screen m-5;
+}
+.table {
+	@apply w-full text-base text-left rtl:text-right text-gray-500 dark:text-gray-400;
+}
+.table-head {
+	@apply text-gray-700 bg-nav-purple bg-opacity-30 dark:bg-gray-700 dark:text-gray-400;
+}
+.col-name {
+	@apply pl-3 pr-6 py-3 w-1/12;
+}
+.col-date {
+	@apply px-6 py-3 w-1/6;
+}
+.one-row {
+	@apply bg-white border-b h-20;
+}
+.kid-name {
+	@apply pl-3 pr-6 py-4 text-gray-900 whitespace-nowrap;
+}
+.normal-value {
+	@apply w-20 my-2 mr-2 p-2 border rounded-md text-center text-gray-900 font-medium;
+}
+.miss-value {
+	@apply w-20 my-2 mr-2 p-2 border rounded-md text-center text-gray-400 font-medium;
+}
+.no-value {
+	@apply w-20 my-2 mr-2 p-2 border rounded-md text-center text-red-600 font-medium;
+}
+.button {
+	@apply w-20 my-2 mr-2 p-2 border rounded-md bg-dark-navy text-center text-gray-100 font-medium;
+}
+</style>
