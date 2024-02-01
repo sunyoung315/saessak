321c7ac250b7f869136a4f3b10bbf1b79affd3b2
leeminwoo <asdfn0@naver.com>
2024-02-01 15:11:40 +0900

    [minwoo]fix:도커 빌드에러 수정

--- recognition/modules/download.py
+++ recognition/modules/download.py
@@ -3,7 +3,7 @@ from deepface import DeepFace
 img_url = "/app/modules/image/image1.png"
 
 result = DeepFace.represent(
-    img1_path=img_url,
+    img_path=img_url,
     align=True,
     detector_backend="opencv",
     enforce_detection=False,
