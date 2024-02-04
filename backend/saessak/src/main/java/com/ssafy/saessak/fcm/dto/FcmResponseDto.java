59dedf9dff9f2a883f6d5858bdbfd477201792e5
0o0mlb <gw07167@naver.com>
2024-02-04 22:10:02 +0900

    [suna]Refactor: FCM DTO Builder 구조 변경

--- backend/saessak/src/main/java/com/ssafy/saessak/fcm/dto/FcmResponseDto.java
+++ backend/saessak/src/main/java/com/ssafy/saessak/fcm/dto/FcmResponseDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.saessak.fcm.dto;
 
+import com.ssafy.saessak.fcm.domain.FcmToken;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,4 +14,16 @@ public class FcmResponseDto {
     private String token;
     private String title;
     private String body;
+
+    public static FcmResponseDto of (
+            final String token,
+            final String title,
+            final String body
+    ) {
+        return FcmResponseDto.builder()
+                .token(token)
+                .title(title)
+                .body(body)
+                .build();
+    }
 }
