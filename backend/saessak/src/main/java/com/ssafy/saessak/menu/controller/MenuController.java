3dc5ca59facce2779a65913609d1220f9bd73a94
0o0mlb <gw07167@naver.com>
2024-02-04 11:21:57 +0900

    [suna]Fix: 식단표 api 사진추가 수정

--- backend/saessak/src/main/java/com/ssafy/saessak/menu/controller/MenuController.java
+++ backend/saessak/src/main/java/com/ssafy/saessak/menu/controller/MenuController.java
@@ -42,9 +42,9 @@ public class MenuController {
     }
 
     @Operation(summary = "식단 메인 사진 추가")
-    @PostMapping(value = "/photo/{menuId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = "multipart/form-data")
-    public ResponseEntity<ResultResponse> insertPhoto(@PathVariable("menuId") Long menuId,
-                                                      @RequestPart("MultipartFile") MultipartFile menuFile) {
+    @PostMapping(value = "/photo", produces = MediaType.APPLICATION_JSON_VALUE, consumes = "multipart/form-data")
+    public ResponseEntity<ResultResponse> insertPhoto(@RequestParam("menuId") Long menuId,
+                                                      @RequestParam("MultipartFile") MultipartFile menuFile) {
         try {
             menuService.insertPhoto(menuId, menuFile);
         } catch (IOException e) {
