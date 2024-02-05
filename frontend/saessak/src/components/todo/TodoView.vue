33a35b59e5ad07c6ce09e1b5f9841c7c94cc2ecb
leeminwoo <asdfn0@naver.com>
2024-02-06 08:53:05 +0900

    [minwoo]Feat: todoList axios

---
+++ frontend/saessak/src/components/todo/TodoView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>todoView</div>
+    <TodoCreate />
+    <div v-for="todo in todoList" :key="todo.todoId">
+        <TodoDetail v-bind:todo=todo />
+
+    </div>
+</template>
+
+<script setup>
+import { BaseTransitionPropsValidators, onMounted, ref, watch } from 'vue'
+import { getTodoList } from '@/api/todo'
+import TodoDetail from '@/components/todo/TodoDetail.vue'
+import TodoCreate from '@/components/todo/TodoCreate.vue'
+
+onMounted(() => {
+    getTodoList((response) => {
+        console.log(response)
+        todoList.value = response.data.data
+        // console.log("success")
+    },
+        (error) => {
+            console.log("fail")
+        })
+
+})
+
+const todoList = ref([])
+
+
+</script>
+
+<style scoped></style>
