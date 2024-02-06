835c70bec11fb30efb402e791445e1b53996a73b
gw07167 <gw07167@naver.com>
2024-02-06 15:43:15 +0900

    [suna]Feat: HttpMethod 추가

--- backend/saessak/src/main/java/com/ssafy/saessak/config/WebMvcConfig.java
+++ backend/saessak/src/main/java/com/ssafy/saessak/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.saessak.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,7 +17,15 @@ class WebMvcConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173",
                         "http://i10a706.p.ssafy.io/",
                         "https://i10a706.p.ssafy.io/",
-                        "http://i10a706.p.ssafy.io:5173");
+                        "http://i10a706.p.ssafy.io:5173")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                        );
 
     }
 }
