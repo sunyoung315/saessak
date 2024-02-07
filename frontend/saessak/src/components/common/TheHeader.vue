93a6ee78384f355d556a16e518988681d014ce3a
gw07167 <gw07167@naver.com>
2024-02-07 15:46:08 +0900

    [suna]Feat: 헤더 레이아웃 아이콘으로 변경

--- frontend/saessak/src/components/common/TheHeader.vue
+++ frontend/saessak/src/components/common/TheHeader.vue
@@ -12,17 +12,22 @@
           ></div>
         </label>
 
-        <RouterLink v-if="isLogin == true" to="/setting">설정</RouterLink>
+        <RouterLink v-if="isLogin == true" to="/setting" class="inline-block">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.5 0L14 3L18 3.5L20 6.5L18 10L20 13.5L18 16.5L14 17L11.5 20H8.5L6 17L2 16.5L0 13.5L2 10L0 6.5L2 3.5L6 3L8.5 0H11.5ZM10.562 1.999H9.437L7.0312 4.88666L3.156 5.37L2.35 6.581L4.3035 10L2.35 13.418L3.156 14.629L7.0312 15.1133L9.436 17.999H10.563L12.9688 15.1133L16.843 14.629L17.649 13.418L15.6965 10L17.649 6.581L16.843 5.37L12.9688 4.88666L10.562 1.999ZM10 6C12.2091 6 14 7.79086 14 10C14 12.2091 12.2091 14 10 14C7.79086 14 6 12.2091 6 10C6 7.79086 7.79086 6 10 6ZM10 8C8.89543 8 8 8.89543 8 10C8 11.1046 8.89543 12 10 12C11.1046 12 12 11.1046 12 10C12 8.89543 11.1046 8 10 8Z" fill="black"/>
+          </svg>
+        </RouterLink>
         <button
           v-if="isLogin == true"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           type="button"
           data-drawer-target="drawer-right-example"
           data-drawer-show="drawer-right-example"
           data-drawer-placement="right"
           aria-controls="drawer-right-example"
         >
-          채팅
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M19 3C20.1046 3 21 3.89543 21 5V17C21 18.1046 20.1046 19 19 19H9L3 22L3 5C3 3.89543 3.89543 3 5 3H19ZM19 5H5V18.468L8.44603 17H19V5ZM11 12V14H7V12H11ZM17 8V10H7V8H17Z" fill="black"/>
+          </svg>
         </button>
         <!-- <RouterLink v-if="isLogin == true" to="/alram">알람</RouterLink> -->
         <!-- 알람 드롭다운 시작해따-->
@@ -356,7 +361,9 @@ const deleteFcmToken = () => {
   )
 }
 
-watch(alarm, (newValue) => {
+watch(alarm, (newValue, oldValue) => {
+  console.log("newValue: ", newValue);
+  console.log("oldValue", oldValue);
   if (newValue) {
     // console.log('checked');
     saveFcmToken()
@@ -408,7 +415,6 @@ const removeAllAlarm = () => {
     // console.log("알람 학부모 전체 삭제");
     deleteAllofParent(curKid.value,
       (response) => {
-        console.log(response);
         getAlarmList();
       }, (error) => {
         console.log(error);
