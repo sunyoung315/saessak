70dae52f176897d480a035a6d295b6853468dcc6
김하은 <nueahx7674@naver.com>
2024-02-07 14:20:54 +0900

    [haeun] Fix: 고정한 공지사항 조회 페이징

--- backend/saessak/src/main/java/com/ssafy/saessak/notice/controller/NoticeController.java
+++ backend/saessak/src/main/java/com/ssafy/saessak/notice/controller/NoticeController.java
@@ -70,7 +70,8 @@ public class NoticeController {
     // PageNation 적용하기 -> 1개 단위
     @Operation(summary = "고정한 공지사항 조회")
     @GetMapping(value = "/fixed/{kidId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResultResponse> getFixedNotice(@PathVariable("kidId") Long kidId, int pageNo) {
+    public ResponseEntity<ResultResponse> getFixedNotice(@PathVariable("kidId") Long kidId,
+                                                         @RequestParam("pageNo") int pageNo) {
         List<NoticeResponseDto> list = noticeService.getAllFixedNotice(kidId, pageNo);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.SUCCESS, list));
     }
