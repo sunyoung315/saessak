99518d895adde7b0ac219405305402ecc1a70c88
gw07167 <gw07167@naver.com>
2024-02-09 17:50:11 +0900

    [suna]Feat: logout api 변경

--- backend/saessak/src/main/java/com/ssafy/saessak/jwt/refreshToken/service/RefreshTokenService.java
+++ backend/saessak/src/main/java/com/ssafy/saessak/jwt/refreshToken/service/RefreshTokenService.java
@@ -4,6 +4,8 @@ import com.ssafy.saessak.exception.code.ExceptionCode;
 import com.ssafy.saessak.exception.model.UnAuthorizedException;
 import com.ssafy.saessak.jwt.refreshToken.domain.RefreshToken;
 import com.ssafy.saessak.jwt.refreshToken.repository.RefreshTokenRepository;
+import com.ssafy.saessak.oauth.service.AuthenticationService;
+import com.ssafy.saessak.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,7 @@ public class RefreshTokenService {
 
     private final RedisTemplate<String, Object> redisTemplate;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final AuthenticationService authenticationService;
 
     @Transactional
     public void saveRefreshToken (final Long userId, final String refreshToken) {
@@ -28,8 +31,9 @@ public class RefreshTokenService {
     }
 
     @Transactional
-    public void deleteRefreshToken(final Long userId) {
-        RefreshToken token = refreshTokenRepository.findById(userId)
+    public void deleteRefreshToken() {
+        User user = authenticationService.getUserByAuthentication();
+        RefreshToken token = refreshTokenRepository.findById(user.getId())
                         .orElseThrow(() -> new UnAuthorizedException(ExceptionCode.REFRESH_TOKEN_NOT_FOUND));
         refreshTokenRepository.delete(token);
     }
