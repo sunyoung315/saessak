69e109562f2e7b08dbecc3716d9c5b16e7e5395b
김하은 <nueahx7674@naver.com>
2024-02-12 01:44:51 +0900

    [haeun] Fix: 학부모 mapping 안 된 아이 제외

--- backend/saessak/src/main/java/com/ssafy/saessak/user/repository/KidRepository.java
+++ backend/saessak/src/main/java/com/ssafy/saessak/user/repository/KidRepository.java
@@ -3,13 +3,18 @@ package com.ssafy.saessak.user.repository;
 import com.ssafy.saessak.user.domain.Classroom;
 import com.ssafy.saessak.user.domain.Kid;
 import com.ssafy.saessak.user.domain.Parent;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface KidRepository extends JpaRepository<Kid, Long> {
+
+    @EntityGraph(attributePaths = {"parent"})
+    Optional<Kid> findByIdAndParentIsNotNull(Long id);
 
     List<Kid> findAllByClassroom(Classroom classroom);
 
