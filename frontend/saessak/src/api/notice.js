e3142b807e356a661f995da944b6f759bea83ac7
sunyoung315 <rlatjsdud315@gmail.com>
2024-02-13 22:37:31 +0900

    [sunyoung]fix: fetchData 무한 호출 오류 수정

--- frontend/saessak/src/api/notice.js
+++ frontend/saessak/src/api/notice.js
@@ -1,58 +1,73 @@
-import { localAxios } from './http'
+import { localAxios } from './http';
 
-const local = localAxios()
+const local = localAxios();
 
-const url = '/notice'
+const url = '/notice';
 
 const config = {
-  headers: {
-    Authorization: 'Bearer ' + localStorage.getItem('accessToken')
-  }
-}
+	headers: {
+		Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
+	},
+};
 
 // noticeList 전부 불러오기
-function noticeListParentAll(kidId, pageNo, sucess, fail) {
-  local.get(`${url}/all/parent/${kidId}?pageNo=${pageNo}`).then(sucess).catch(fail)
-}
+const noticeListParentAll = async (kidId, pageNo, sucess, fail) => {
+	await local
+		.get(`${url}/all/parent/${kidId}?pageNo=${pageNo}`)
+		.then(sucess)
+		.catch(fail);
+};
 
-function noticeListTeacherAll(param, sucess, fail) {
-  const config = {
-    headers: {
-      Authorization: 'Bearer ' + localStorage.getItem('accessToken')
-    },
-    params: {
-      pageNo: param
-    }
-  }
-  local.get(`${url}/all/teacher`, config).then(sucess).catch(fail)
-}
+const noticeListTeacherAll = async (param, sucess, fail) => {
+	const config = {
+		headers: {
+			Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
+		},
+		params: {
+			pageNo: param,
+		},
+	};
+	await local.get(`${url}/all/teacher`, config).then(sucess).catch(fail);
+};
 
-function parentFix(param, sucess, fail) {
-  local.post(`${url}/fixed/parent/add`, JSON.stringify(param)).then(sucess).catch(fail)
-}
+const parentFix = async (param, sucess, fail) => {
+	await local
+		.post(`${url}/fixed/parent/add`, JSON.stringify(param))
+		.then(sucess)
+		.catch(fail);
+};
 
-function parentNotFix(param, success, fail) {
-  local.delete(`${url}/fixed/parent/delete`, { data: param }).then(success).catch(fail)
-}
+const parentNotFix = async (param, success, fail) => {
+	await local
+		.delete(`${url}/fixed/parent/delete`, { data: param })
+		.then(success)
+		.catch(fail);
+};
 
-function teacherFix(param, sucess, fail) {
-  local.post(`${url}/fixed/teacher/add?noticeId=${param}`, null, config).then(sucess).catch(fail)
-}
+const teacherFix = async (param, sucess, fail) => {
+	await local
+		.post(`${url}/fixed/teacher/add?noticeId=${param}`, null, config)
+		.then(sucess)
+		.catch(fail);
+};
 
-function teacherNotFix(param, success, fail) {
-  local.delete(`${url}/fixed/teacher/delete?noticeId=${param}`, config).then(success).catch(fail)
-}
+const teacherNotFix = async (param, success, fail) => {
+	await local
+		.delete(`${url}/fixed/teacher/delete?noticeId=${param}`, config)
+		.then(success)
+		.catch(fail);
+};
 
 function noticeDetail(noticeId, sucess, fail) {
-  local.get(`${url}/detail/${noticeId}`, config).then(sucess).catch(fail)
+	local.get(`${url}/detail/${noticeId}`, config).then(sucess).catch(fail);
 }
 
 export {
-  noticeListParentAll,
-  noticeListTeacherAll,
-  noticeDetail,
-  parentNotFix,
-  parentFix,
-  teacherNotFix,
-  teacherFix
-}
+	noticeListParentAll,
+	noticeListTeacherAll,
+	noticeDetail,
+	parentNotFix,
+	parentFix,
+	teacherNotFix,
+	teacherFix,
+};
